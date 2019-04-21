af304f3cd78944c7bec586c5177c1cfecff166d6
Sumit Saha <sumit.saha@appdynamics.com>
2019-04-21 21:27:35 +0530

    adding sample home for angular

--- scrumboard/api.py
+++ scrumboard/api.py
@@ -1,11 +1,12 @@
+from .serializers import ListSerializer, CardSerializer
+from .models import List, Card
 from rest_framework.generics import ListAPIView
 
-from.serializers import ListSerializer, CardSerializer
-from .models import List, Card
 
 class ListApi(ListAPIView):
     queryset = List.objects.all()
     serializer_class = ListSerializer
+
 
 class CardApi(ListAPIView):
     queryset = Card.objects.all()
