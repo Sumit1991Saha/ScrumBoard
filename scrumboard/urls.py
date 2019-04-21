a04500a81374f09e523fd40c305476c123028d25
Sumit Saha <sumit.saha@appdynamics.com>
2019-04-21 21:31:04 +0530

    some file renaming

--- scrumboard/urls.py
+++ scrumboard/urls.py
@@ -6,5 +6,6 @@ from .api import ListApi, CardApi
 urlpatterns = [
     url(r'^lists$', ListApi.as_view()),
     url(r'^cards$', CardApi.as_view()),
-    url(r'^home$', TemplateView.as_view(template_name="scrumboard/home.html")),
+    url(r'^home1$', TemplateView.as_view(template_name="scrumboard/home1.html")),
+    url(r'^home2$', TemplateView.as_view(template_name="scrumboard/home2.html")),
 ]
