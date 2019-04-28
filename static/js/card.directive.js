750e1287263268f1040fe40c918256a692f380ac
Sumit Saha <sumit.saha@appdynamics.com>
2019-04-28 22:09:56 +0530

    adding functionality to move data from 1 list to other

--- static/js/card.directive.js
+++ static/js/card.directive.js
@@ -9,22 +9,48 @@
             restrict: 'E',
             controller: ['$scope', '$http', function($scope, $http) {
                 var url='/scrumboard/cards/' + $scope.card.id + '/';
+                $scope.destList = $scope.list;
+
                 $scope.update = function () {
-                    $http.put(url, $scope.card);
+                    return $http.put(
+                        url, 
+                        $scope.card
+                    );
                 };
-                 //This is used to wait for 500 milli seconds 
+                
+                //This is used to wait for 500 milli seconds 
                  // before send the rest update call 
                  // else each key stroke would send an update request to the server
                 $scope.modelOptions = { 
                     debounce: 500
                 };
+
+                function removeCardFromList(card, list) {
+                    var cards = list.cards;
+                    cards.splice(
+                        cards.indexOf(card), 
+                        1
+                    );
+                }
+                
                 $scope.delete = function() {
                     $http.delete(url).then(function() {
-                        var cards = $scope.list.cards;
-                        cards.splice(cards.indexOf($scope.card), 1);
+                        removeCardFromList($scope.card, $scope.list);
                         alert($scope.card.title + " deleted succesfully");
                     })
                 }
+
+                $scope.move = function() {
+                    if ($scope.destList == undefined) {
+                        return;
+                    }
+                    $scope.card.list = $scope.destList.id;
+                    $scope.update().then( function() {
+                        removeCardFromList($scope.card, $scope.list);
+                        $scope.destList.cards.push($scope,card);
+                    });
+                }
+
             }]
         };
     }
